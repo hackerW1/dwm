d5fd028514039638d179b460f8265adef6be321f
hackerW1 <ytanmay37@gmail.com>
2022-03-03 18:55:43 +0530

    firefox on www

--- config.h
+++ config.h
@@ -31,8 +31,8 @@ static const Rule rules[] = {
 	 */
 	/* class     instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
 	{ "Gimp",    NULL,     NULL,           0,         1,          0,           0,        -1 },
-	{ "firefox", NULL,     NULL,           0,         0,          0,          -1,        -1 },
-	{ "st-256color",      NULL,     NULL,           0,         0,          1,           0,        -1 },
+	{ "firefox", NULL,     NULL,           1 << 2,    0,          0,          -1,        -1 },
+	{ "st-256color",      NULL,     NULL,           0,         0,         0,         1,           0,        -1 },
 	{ NULL,      NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
 };
 
